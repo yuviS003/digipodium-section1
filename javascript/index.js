34b982b91c0a3eeee75be15042fdc743a5fb902f
Yuvraj Singh <yuvi7860808034@gmail.com>
2024-02-16 13:54:24 +0530

    javascript started with variables scopes

---
+++ javascript/index.js
@@ -0,0 +1,28 @@
+console.log(typeof "hey");
+
+a = 10;
+console.log(typeof a);
+
+empty = null;
+console.log(typeof empty);
+
+myname = "yuvraj";
+console.log(myname);
+
+console.log(2 == "2");
+console.log(2 === "2");
+
+console.log("hey" == "HEY");
+console.log("hey" === "HEY");
+
+console.log(true == "true");
+console.log(true === "true");
+
+console.log(1 == true);
+console.log(1 === true);
+
+console.log(0 == false);
+console.log(0 === false);
+
+// exponents
+console.log(4 ** 3);
---
+++ javascript/keywords.js
@@ -0,0 +1,6 @@
+if (34 % 2 === 0) {
+  console.log("Even");
+  a = "hey";
+} else console.log("Odd");
+
+console.log(a);
